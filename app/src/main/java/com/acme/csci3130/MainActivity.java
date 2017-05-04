1950a4e1ce42b688b239996c3c40c920f8bfc396
asok <asok.kalidass@gmail.com>
2017-05-04 01:09:18 -0300

    Added new requirements as per requirement in assignment 1

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,10 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickMe_Click(View target) {
+        TextView displayText =new TextView(this);
+        EditText editText = (EditText) findViewById(R.id.txtInputBox);
+        String inputString = editText.getText().toString();
+        displayText = (TextView)findViewById(R.id.helloText);
+        displayText.setText(inputString);
     }
 }
